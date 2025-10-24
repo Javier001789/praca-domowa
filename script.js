9bfd6a84a374c744fb8ae726ce1620d8a5bd8d99
Javier001789 <makscool0998gmail.com>
2025-10-24 08:10:51 +0200

    finish all its good

--- script.js
+++ script.js
@@ -1,62 +1,70 @@
-function zaplanujMission(nazwaMisij,typMisji){
+function zaplanujMission(nazwaMisij, typMisji) {
 
 
-    return{
-    
-    nazwa: nazwaMisij,
-    typ: typMisji, 
-    zaloga: ["Szkiper", "Kowalski"],
-    dystans: 10, 
-    celeBadawcze: ["Poszukiwanie wody"],
-    ladownia: {
-        narzedzia: ["flashlight"],
-        zapasy: 100 
+    return {
+
+        nazwa: nazwaMisij,
+        typ: typMisji,
+        zaloga: ["Szkiper", "Kowalski"],
+        dystans: 10,
+        celeBadawcze: ["Poszukiwanie wody"],
+        ladownia: {
+            narzedzia: ["flashlight"],
+            zapasy: 100
+        }
+
     }
 
-    }
+}
+const misja = zaplanujMission("Misja na Marsa", "Badawcza")
+
+function dodajCzlonkaZalogi(zaplanujMission, zaloga) {
+    zaplanujMission.zaloga.push(zaloga);
 
 }
-const misja = zaplanujMission ("Misja na Marsa","Badawcza")
-
-function dodajCzlonkaZalogi(nazwaMisji, czlonek){
-nazwaMisji.zaloga.push(czlonek);
-}
-function zaladujLadownia(misja, ladownia){
-misja.ladownia.push(ladownia)
+function zaladujLadownia(zaplanujMission, ladownia) {
+    zaplanujMission.ladownia.narzedzia.push(ladownia)
 
 }
-function przemierzDystansu(misja, odleglosc){
-    misja.dystans = odleglosc + 20
+
+function przemierzDystansu(misja) {
+    const Odleglosc = misja.dystans
+    misja.dystans = Odleglosc + 20
 }
 
-function iloscZapasow(misja){
-    const Ilosc = misja.zapasy
-zapasy.dystans = Ilosc = + 20
+function iloscZapasow(misja, liczba) {
+    //const ilosc = misja.ladownia.zapasy
+    // ladownia.zapasy.dystans = Ilosc = + 20
+    misja.ladownia.zapasy += liczba
 }
 
 
 
 
-function showMisje(misja){
+function showMisje(misja) {
     console.log(
         `
   *** RAPORT MISJI: ${misja.nazwa} ***
 Typ misji: ${misja.typ}
 Przebyty dystans: ${misja.dystans}
-Pozostałe zapasy: ${misja.zapasy}
+Pozostałe zapasy: ${misja.ladownia.zapasy}
 
 --- ZAŁOGA ---
 ${misja.zaloga.map((zaloga) => "- " + zaloga).join("\n")}
 
 --- SPRZĘT W ŁADOWNI ---
-
+${misja.ladownia.narzedzia.map((ladownia) => "- " + ladownia).join("\n")}
 `
-        
-        
+
+
     )
 }
 
 
+showMisje(misja)
+przemierzDystansu(misja)
+showMisje(misja)
+iloscZapasow(misja, 20)
 showMisje(misja)
 
 
